149b6addf75ecadd7c3a80d03bf812c0bbf77935
itsdevpawan <itsdevpawan@gmail.com>
2020-08-11 12:05:43 +0530

    Updates

---
+++ precache-manifest.cd2927b49da3dbd15d3e86076ec98cc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11990c030ac04fc6fcef6a93034e49aa",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "c7ce941093513ff73ac5",
+    "url": "/Covid-19/static/css/main.edac2647.chunk.css"
+  },
+  {
+    "revision": "a3a06f83e950c9cfc689",
+    "url": "/Covid-19/static/js/2.4c036d69.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.4c036d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ce941093513ff73ac5",
+    "url": "/Covid-19/static/js/main.ac3e0f01.chunk.js"
+  },
+  {
+    "revision": "843f1e60e5802074d15a",
+    "url": "/Covid-19/static/js/runtime-main.bb5c8cdf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  }
+]);
